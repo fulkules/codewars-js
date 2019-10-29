84190627f1bf12b298a697c45625c4a07fa8739e
Barry Fulk <barry@iltech.com>
2019-10-29 10:19:04 -0600

    Human Readable Time refactor added

--- kata-5/Human-Readable-Time/index.js
+++ kata-5/Human-Readable-Time/index.js
@@ -31,4 +31,12 @@ function formatTime(seconds){
   return `${readableHour}:${readableMinutes}:${readableSeconds}`
 }
 
+// IE compatible refactor
+//function humanReadable(seconds) {
+    // var pad = function(x) { return (x < 10) ? "0"+x : x; }
+    // return pad(parseInt(seconds / (60*60))) + ":" +
+    //        pad(parseInt(seconds / 60 % 60)) + ":" +
+    //        pad(seconds % 60)
+//   }
+
 module.exports = formatTime;
