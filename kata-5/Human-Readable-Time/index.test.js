6ab6bd69eca9dd8be6c7125d4d28ff3c60e69319
Barry Fulk <barry@iltech.com>
2019-10-29 10:12:54 -0600

    updated tests

--- kata-5/Human-Readable-Time/index.test.js
+++ kata-5/Human-Readable-Time/index.test.js
@@ -1,11 +1,11 @@
 const formatTime = require('./index');
 
-describe('examples', function() {
+describe('formatTime', function() {
     it('should format correctly', function() {
-      Test.assertEquals(humanReadable(0), '00:00:00', 'humanReadable(0)');
-      Test.assertEquals(humanReadable(5), '00:00:05', 'humanReadable(5)');
-      Test.assertEquals(humanReadable(60), '00:01:00', 'humanReadable(60)');
-      Test.assertEquals(humanReadable(86399), '23:59:59', 'humanReadable(86399)');
-      Test.assertEquals(humanReadable(359999), '99:59:59', 'humanReadable(359999)');
+      expect(formatTime(0)).toStrictEqual('00:00:00');
+      expect(formatTime(5)).toStrictEqual('00:00:05');
+      expect(formatTime(60)).toStrictEqual('00:01:00');
+      expect(formatTime(86399)).toStrictEqual('23:59:59');
+      expect(formatTime(359999)).toStrictEqual('99:59:59');
     });
   });
