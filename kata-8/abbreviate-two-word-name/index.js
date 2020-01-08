a7b7e26f3d4d47ff8448faf3fa2d404b8be8829c
Barry Fulk <barry@iltech.com>
2020-01-08 07:20:27 -0700

    abbreviate two word name with tests

---
+++ kata-8/abbreviate-two-word-name/index.js
@@ -0,0 +1,25 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// Patrick Feeney => P.F
+
+// [Patrick, Freeny]
+// [P, F]
+// P.F
+
+function abbrevName(name) {
+    let nameArray = name.split(" ");
+    return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+}
+
+// refactor chaining HOFs
+// function abbrevName(name) { 
+//     return name.split(' ').map(name => name[0].toUpperCase()).join('.');
+// }
+
+module.exports = abbrevName;
