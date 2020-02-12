6aa6c93b951d0e718fce3724e7e7936353e0af9a
Barry Fulk <barry@iltech.com>
2020-02-12 07:05:37 -0700

    sum two lowest integers with tests

--- kata-7/list-filtering/index.test.js
+++ kata-7/list-filtering/index.test.js
@@ -1,7 +1,7 @@
 const filter_list = require('./index');
 
 describe(filter_list, () => {
-    it ('should return)', () => {
+    it ('should return a list with strings removed', () => {
         expect(filter_list([1,2,'a','b'])).toStrictEqual([1,2]);
         expect(filter_list([1,'a','b',0,15])).toStrictEqual([1,0,15]);
         expect(filter_list([1,2,'aasf','1','123',123])).toStrictEqual([1,2,123]);
