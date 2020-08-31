b4c1efd533c7a9181c77ce0e2d7a4c589ba3a142
Barry Fulk <barry@iltech.com>
2020-08-31 08:33:14 -0600

    ordered count of characters with tests

---
+++ kata-7/ordered-count-of-characters/index.test.js
@@ -0,0 +1,10 @@
+const orderedCount = require('./index');
+
+describe(orderedCount, () => {
+    it('should return a number', () => {
+        expect(orderedCount('abracadabra')).toBeInstanceOf(Array);
+    });
+    it('should return an array of tuples with the ordered count of characters', () => {
+        expect(orderedCount('abracadabra')).toStrictEqual([['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]);
+    });
+});
