479b54be587944ed16b8c99934a0ce0c82cbd6cc
Barry Fulk <barry@iltech.com>
2020-09-20 11:22:20 -0600

    complete the pattern #2 with tests

---
+++ kata-7/complete-the-pattern-#2/index.test.js
@@ -0,0 +1,12 @@
+const pattern = require('./index');
+
+describe(pattern, () => {
+    it('should return an array', () => {
+        expect(typeof pattern(1)).toBe('string');
+    });
+    it('should return', () => {
+        expect(pattern(1)).toStrictEqual('1');
+        expect(pattern(2)).toStrictEqual('21\n2');
+        expect(pattern(5)).toStrictEqual('54321\n5432\n543\n54\n5');
+    });
+});
