4c508f83c137a073510f7f1d2fae192d494e433c
Barry Fulk <barry@iltech.com>
2020-10-18 10:19:26 -0600

    beginning series 4 cockroach with tests

---
+++ kata-8/beginning-series-#4-cockroach/index.test.js
@@ -0,0 +1,11 @@
+const cockroachSpeed = require('./index');
+
+describe('cockroachSpeed', function() {
+    it('should return a number', function() {
+        expect(typeof cockroachSpeed(1.08)).toBe('number');
+    });
+    test('returns the cm per second rounded down', function() {
+        expect(cockroachSpeed(1.08)).toStrictEqual(30);
+        expect(cockroachSpeed(0)).toStrictEqual(0);
+    });
+})
