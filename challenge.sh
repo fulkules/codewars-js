34a1da1cfc33ef5c0eb1ac8f0576c5107f4b92ed
Barry Fulk <barry@iltech.com>
2021-02-09 08:12:41 -0700

    another codewars kata challenge

---
+++ challenge.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "---------------------------------------------"
+echo "Preparing merge to git"
+
+echo '
+	git add .
+	git commit -m 'another codewars kata challenge'
+	git push
+	git checkout master
+	git pull
+	git merge challenge
+	git checkout challenge
+'
+
+#### NOTE IF YOU ADD A LINE HERE ADD IT ABOVE JUST FOR BETTER MESSAGING -BJM 3/7/2018
+
+git add .
+	git commit -m 'another codewars kata challenge'
+	git push
+	git checkout master
+	git pull
+	git merge challenge
+	git checkout challenge
+
+echo "---------------------------------------------"
+echo "Finished merging challenge to master."
+echo "---------------------------------------------"
