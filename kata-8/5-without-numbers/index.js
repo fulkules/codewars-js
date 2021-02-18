14d6a6f5920ca06d9ed9d4b033b954cd1aeb6b47
Barry Fulk <barry@iltech.com>
2021-02-18 07:55:18 -0700

    kata challenge

---
+++ kata-8/5-without-numbers/index.js
@@ -0,0 +1,19 @@
+// Write a function that always returns 5
+//
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+//
+// Good luck :)
+
+function unusualFive(){
+    return 'joker'.length;
+}
+
+// function unusualFive() {
+// return "".charCodeAt();
+// }
+
+// function unusualFive() {
+//   return Math.hypot(Math.ceil(Math.PI), Math.ceil(Math.E))
+// }
+
+module.exports = unusualFive;
