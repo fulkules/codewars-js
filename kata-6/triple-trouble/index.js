e3898de23aebcdd9601446b204d199cf7b1abaaf
Barry Fulk <barry@iltech.com>
2021-04-28 08:30:30 -0600

    kata challenge

--- kata-6/triple-trouble/index.js
+++ kata-6/triple-trouble/index.js
@@ -17,11 +17,49 @@
 // tripledouble(666789, 12345667) == 1
 
 // fn(num, num) => num
-// need to see if the first num has 3 consecutive chars
+// need to see if the first num has 3 consecutive nums
+    // convert to string and iterate
+    // compare i, i + 1, i + 2
+    // if all the same grab the value at i
 // 2nd num must have 2 of the same consecutive nums
+    // convert to string and iterate
+    // compare i, i + 1
+    // if both match the value of triple
+        // return 1 else 0
 
 function tripledouble(n1, n2){
-
+    let str = String(n1), str2 = String(n2), val = '';
+    for(let i = 0; i < str.length; i++){
+        if(str.charAt(i) === str.charAt(i + 1) && str.charAt(i + 1) === str.charAt(i + 2)){
+            val = str.charAt(i);
+            for(let j = 0; j < str2.length; j++){
+                if((str2.charAt(j) === str2.charAt(j + 1)) && str2.charAt(j) === val){
+                    return 1;
+                }
+            }
+        }
+    }
+    return 0;
 }
+
+// refactor with RegExp
+// function tripledouble(num1, num2) {
+//   for (let i = 0; i < 10; i++) {
+//     if (new RegExp(`${i}{3}`).test(num1) && new RegExp(`${i}{2}`).test(num2)) {
+//       return 1;
+//     }
+//   }
+//   return 0;
+// }
+
+// another interesting solution
+// function tripledouble(num1, num2) {
+//   let nums = [...Array(10).keys()];
+//
+//   return +nums.some(num =>
+//     num1.toString().includes(num.toString().repeat(3)) &&
+//     num2.toString().includes(num.toString().repeat(2))
+//   );
+// }
 
 module.exports = tripledouble;
