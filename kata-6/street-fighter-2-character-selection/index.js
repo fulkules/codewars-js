e1929d1703a6aad871ca0f6ba88268bd13d81cf4
Barry Fulk <barry@iltech.com>
2021-06-21 08:34:59 -0600

    kata challenge

--- kata-6/street-fighter-2-character-selection/index.js
+++ kata-6/street-fighter-2-character-selection/index.js
@@ -68,3 +68,69 @@
 // Result:
 //
 // ['E.Honda', 'Chun Li', 'Ken', 'M.Bison', 'Sagat', 'Dhalsim', 'Sagat']
+
+
+
+function streetFighterSelection(fighters, position, moves){
+    if(!moves.length) return [];
+    let selected = [];
+    for(let i = 0; i < moves.length; i++){
+        switch(moves[i]){
+            case 'right':
+                if(position[1] === fighters[0].length - 1){
+                    position[1] = 0;
+                } else {
+                    position[1] += 1;
+                }
+                selected.push(fighters[position[0]][position[1]]);
+                break;
+            case 'left':
+                if(position[1] === 0){
+                    position[1] = fighters[0].length - 1;
+                } else {
+                    position[1] -= 1;
+                }
+                selected.push(fighters[position[0]][position[1]]);
+                break;
+            case 'up':
+                if(position[0] > 0){
+                    position[0] -= 1;
+                }
+                selected.push(fighters[position[0]][position[1]]);
+                break;
+            case 'down':
+                if(position[0] < 1){
+                    position[0] += 1;
+                }
+                selected.push(fighters[position[0]][position[1]]);
+                break;
+        }
+    }
+    return selected;
+}
+
+// best practice
+// function streetFighterSelection(fighters, position, moves){
+//   let result = [];
+//
+//   moves.forEach(function(move) {
+//     if (move === "up") {
+//       position[0] = 0;
+//     }
+//     else if (move === "down") {
+//       position[0] = 1;
+//     }
+//     else if (move === "right") {
+//       position[1] = (position[1] === 5) ? 0 : position[1] + 1;
+//     }
+//     else if (move === "left") {
+//       position[1] = (position[1] === 0) ? 5 : position[1] - 1;
+//     }
+//
+//     result.push(fighters[position[0]][position[1]]);
+//   });
+//
+//   return result;
+// }
+
+module.exports = streetFighterSelection;
